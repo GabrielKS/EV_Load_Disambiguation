dc3ade3039708fcaf939687ff04587c376ae2557
gks-spa <gks-spa@users.noreply.github.com>
2019-05-20 16:23:43 -0500

    Save all generated data instead of just the input

--- get_input.py
+++ get_input.py
@@ -3,6 +3,11 @@ import numpy as np
 import random
 from collections import OrderedDict
 
+# "combined": The combination of the EV load and the baseline power consumption. The input.
+# "load": The EV load. Output 1.
+# "households": The household-to-vehicle map. Output 2.
+# "params": Parameters. Secondary input.
+
 def load_time_series(path):
     print("loading")
     var = pd.read_csv(path)
@@ -10,20 +15,47 @@ def load_time_series(path):
     var.drop(columns="Time", inplace=True)
     return var
 
-def get_input(vehicles_L1, vehicles_L2, timestep, random_seed=None):
-    if random_seed == None: return simulate_input(vehicles_L1, vehicles_L2, timestep)
-    path = "input/load_"+str(timestep)+"_"+str(vehicles_L1)+"_"+str(vehicles_L2)+"_"+str(random_seed)+".csv"
-    try:
-        return load_time_series(path)
-    except FileNotFoundError:
-        input, _, _ = simulate_input(vehicles_L1, vehicles_L2, timestep)
-        input.to_csv(path)
-        return input
+def pack_data(combined, load, households, params, get_combined=True, get_load=True, get_households=True, get_params=True):
+    data = {}
+    if get_combined: data["combined"] = combined
+    if get_load: data["load"] = load
+    if get_households: data["households"] = households
+    if get_params: data["params"] = params
+    return data
+
+def unpack_data(data):
+    if "combined" not in data: data["combined"] = None
+    if "load" not in data: data["load"] = None
+    if "households" not in data: data["households"] = None
+    if "params" not in data: data["params"] = None
+    return data["combined"], data["load"], data["households"], data["params"]
+
+def get_data(vehicles_L1, error_L1, vehicles_L2, error_L2, timestep, random_seed=None, get_combined=True, get_load=True, get_households=True, get_params=True):
+    combined, load, households, params = None, None, None, None
+    if random_seed == None:
+        combined, load, households, params = unpack_data(simulate_data(vehicles_L1, vehicles_L2, timestep))
+    else:
+        path_combined = "simulated/combined_"+str(timestep)+"_"+str(vehicles_L1)+"_"+str(vehicles_L2)+"_"+str(random_seed)+".csv"
+        path_load = "simulated/load_"+str(timestep)+"_"+str(vehicles_L1)+"_"+str(vehicles_L2)+"_"+str(random_seed)+".csv"
+        path_households = "simulated/households_"+str(timestep)+"_"+str(vehicles_L1)+"_"+str(vehicles_L2)+"_"+str(random_seed)+".csv"
+        path_params = "simulated/params_"+str(timestep)+"_"+str(vehicles_L1)+"_"+str(vehicles_L2)+"_"+str(random_seed)+".csv"
+        try:
+            if get_combined: combined = load_time_series(path_combined)
+            if get_load: load = load_time_series(path_load)
+            if get_households: households = pd.read_csv(path_households, index_col=0)
+            if get_params: params = pd.read_csv(path_params, index_col=0, header=None, squeeze=True)
+        except FileNotFoundError:
+            combined, load, households, params = unpack_data(simulate_data(vehicles_L1, error_L1, vehicles_L2, error_L2, timestep))
+            combined.to_csv(path_combined)
+            load.to_csv(path_load)
+            households.to_csv(path_households)
+            params.to_csv(path_params, header=False)
+    return pack_data(combined, load, households, params, get_combined, get_load, get_households, get_params)
 
 def load_resample(path, key, distribution, freq, vehicles_to_households):
     var = load_time_series(path)
     var = var[vehicles_to_households["Vehicle"].iloc[distribution == key]] #Get rid of the data we don't need
-    print("adding resolution")
+    print("fuzzing")
     for s in var:
         print("\t"+s)
         col = var[s].copy()
@@ -41,15 +73,22 @@ def load_resample(path, key, distribution, freq, vehicles_to_households):
                 else: col.iloc[i+1] = max*offset
                 # col.iloc[i] = -2
         var[s] = col
-    #var = var.resample(freq).mean()
+    var = var.resample(freq).mean()
     print(var)
     return var
 
-def simulate_input(vehicles_L1, vehicles_L2, timestep, random_seed=None):
+def simulate_data(vehicles_L1, error_L1, vehicles_L2, error_L2, timestep, random_seed=None):
     print("Simulating input from scratch…")
+    params = pd.Series({"vehicles_L1": vehicles_L1, "vehicles_L2": vehicles_L2, "error_L1": error_L1, "error_L2": error_L2})
+    random.seed(a=random_seed)
+    vehicles_L1 += error_L1*(random.random()*2-1)
+    vehicles_L2 += error_L2*(random.random()*2-1)
+    vehicles_L1 = int(round(vehicles_L1))
+    vehicles_L2 = int(round(vehicles_L2))
+    vehicles_L1 = max(vehicles_L1, 0)
+    vehicles_L2 = max(vehicles_L2, 0)
     vehicles_to_households = pd.read_csv("raw_data/vehicles.csv")
     distribution = np.array(["L1"] * vehicles_L1 + ["L2"] * vehicles_L2 + ["Gas"] * (len(vehicles_to_households) - vehicles_L1 - vehicles_L2))
-    random.seed(a=random_seed)
     random.shuffle(distribution)
 
     baseline = load_time_series("raw_data/Residential-Profiles.csv")
@@ -82,18 +121,19 @@ def simulate_input(vehicles_L1, vehicles_L2, timestep, random_seed=None):
             households_to_vehicles.loc[households_to_vehicles["Household"] == household, distribution[index]] += 1
             if distribution[index] == "L1": load[household] += load_L1[vehicle]
             else: load[household] += load_L2[vehicle]
+    return {"combined": baseline+load, "load": load, "households": households_to_vehicles, "params": params}
 
-    return baseline+load, load, households_to_vehicles
-
-def main():
+def main(random_seed=0):
     #Overall EV market share: https://evadoption.com/ev-market-share/; let's say 25% like in Palo Alto
     #Can't find level 1 vs level 2 stats beyond the fact that L2 is much more convenient; assuming 30% L1 and 60% L2
     #30-minute intervals for load data is on the optimistic end of plausible
     #Using random seed of 0 for reproducibility
     n_vehicles = len(pd.read_csv("raw_data/vehicles.csv"))
-    n_L1 = int(n_vehicles * 0.75 * 0.3)
-    n_L2 = int(n_vehicles * 0.75 * 0.6)
-    print(simulate_input(n_L1, n_L2, 3, 0))
+    n_L1 = n_vehicles * 0.75 * 0.3
+    n_L2 = n_vehicles * 0.75 * 0.6
+    d_L1 = n_L1*0.05
+    d_L2 = n_L2*0.05
+    print(get_data(n_L1, d_L1, n_L2, d_L2, 3, random_seed))
 
 if __name__ == "__main__":
     main()
